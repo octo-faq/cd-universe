470ad6b6d338b5ca557224dab98c8c93e7f63ae7
Daniel Figucio <745341+affrae@users.noreply.github.com>
2021-02-05 07:47:02 +1100

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,6 @@ const api = require('../utils/api');
 
 function SelectLanguage(props) { 
   const languages = ['All', 'JavaScript', 'Java', 'CSharp', 'Python', 'Go', 'Rust'];
-  
 //  const languages = ['All', 'Go', 'Rust', 'Python'];
 
   return (
